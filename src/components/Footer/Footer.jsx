4714de405cd6872a43c6cc97d9ff9d6f4494a078
heera9331 <heera9331@gmail.com>
2024-02-22 18:59:17 +0530

    style issue fixed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex w-full bg-gray-100 border-t text-gray-600 border-black border-opacity-25 dark:bg-gray-900 h-[100px] text-center items-center font-semibold justify-center">
+    <footer className="flex w-full bg-gray-100 border-t text-gray-600 border-black border-opacity-25 dark:bg-gray-900 dark:text-white h-[100px] text-center items-center font-semibold justify-center">
       <h2>Design & Developed by - Heera Singh Lodhi</h2>
     </footer>
   );
